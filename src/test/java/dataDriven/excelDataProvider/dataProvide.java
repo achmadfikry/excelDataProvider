e3fe12789b1d564b5dfdaa3852ea051e76c18848
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-08-09 22:45:03 +0700

    Initiate integration data provider to excel

--- src/test/java/dataDriven/excelDataProvider/dataProvide.java
+++ src/test/java/dataDriven/excelDataProvider/dataProvide.java
@@ -1,20 +1,49 @@
 package dataDriven.excelDataProvider;
 
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class dataProvide {
 
     @Test(dataProvider = "driveTest")
-    public void testCaseData(String greeting, String communication, int id){
+    public void testCaseData(String greeting, String communication, String id){
         System.out.println(greeting);
         System.out.println(communication);
         System.out.println(id);
     }
 
     @DataProvider(name="driveTest")
-    public Object[][] getData(){
-        Object[][] data = {{"hello","text",1},{"bye","message",143},{"solo","call",453}};
+    public Object[][] getData() throws IOException {
+//        Object[][] data = {{"hello","text","1"},{"bye","message","143"},{"solo","call","453"}};
+
+        //tell where the file is located
+        FileInputStream fis = new FileInputStream("/Users/telkomdev/Java/ExcelDriven/src/main/java/Framework/Main.java");
+
+        //define workbook intance, because excel considered as a workbook by Apache POI API
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+
+        //ambil sheet
+        XSSFSheet sheet = wb.getSheetAt(0);
+
+        //get number of total row present in the sheet
+        int rowCount = sheet.getPhysicalNumberOfRows();
+
+        //get first row
+        XSSFRow row  = sheet.getRow(0);
+
+        //get last cell number which is the number of coloumn present
+        int colcount = row.getLastCellNum();
+
+        //iterate every arrow & throw / create into one array
+        Object data[][] = new Object[rowCount - 1][colcount];
+
 
         return data;
     }
