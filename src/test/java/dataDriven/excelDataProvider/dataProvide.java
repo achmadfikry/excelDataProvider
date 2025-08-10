60f064d650e6566cb18e8a559faba1081e694ab5
Achmad Fikry <achmadfikryy08@gmail.com>
2025-08-10 17:48:01 +0700

    Integrating Excel to Dataprovider

--- src/test/java/dataDriven/excelDataProvider/dataProvide.java
+++ src/test/java/dataDriven/excelDataProvider/dataProvide.java
@@ -1,5 +1,7 @@
 package dataDriven.excelDataProvider;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -12,6 +14,8 @@ import java.io.IOException;
 
 public class dataProvide {
 
+    DataFormatter formatter = new DataFormatter();
+
     @Test(dataProvider = "driveTest")
     public void testCaseData(String greeting, String communication, String id){
         System.out.println(greeting);
@@ -21,10 +25,17 @@ public class dataProvide {
 
     @DataProvider(name="driveTest")
     public Object[][] getData() throws IOException {
-//        Object[][] data = {{"hello","text","1"},{"bye","message","143"},{"solo","call","453"}};
+        //Object[][] data = {{"hello","text","1"},{"bye","message","143"},{"solo","call","453"}};
+        //Object data[][] = new Object[2][3];
+        //data[0][0]="hello";
+        //data[0][1]="text;
+        //data[0][2]="1";
+        //data[1][0]=bye";
+        //data[1][1]="message";
+        //data[1][2]="143";
 
         //tell where the file is located
-        FileInputStream fis = new FileInputStream("/Users/telkomdev/Java/ExcelDriven/src/main/java/Framework/Main.java");
+        FileInputStream fis = new FileInputStream("D:/Storage/Fikry/Course & Bootcamp/QA/Selenium Web Driver Java/excelDataProvider/src/test/java/dataDriven/excelDataProvider/Book 1.xlsx");
 
         //define workbook intance, because excel considered as a workbook by Apache POI API
         XSSFWorkbook wb = new XSSFWorkbook(fis);
@@ -39,15 +50,16 @@ public class dataProvide {
         XSSFRow row  = sheet.getRow(0);
 
         //get last cell number which is the number of coloumn present
-        int colcount = row.getLastCellNum();
+        int colCount = row.getLastCellNum();
 
         //iterate every arrow & throw / create into one array
-        Object data[][] = new Object[rowCount-1][colcount];
+        Object data[][] = new Object[rowCount-1][colCount];
 
-        for (int i = 0; i < rowCount; i++) {
-            row = sheet.getRow(i+1);
-            for (int j = 0; j < colcount; j++) {
-                row.getCell(j);
+        for (int i = 0; i < rowCount-1; i++) {
+            row = sheet.getRow(i+1); //get data from row
+            for (int j = 0; j < colCount; j++) {
+                XSSFCell cell = row.getCell(j); //get data from cell
+                data[i][j] = formatter.formatCellValue(cell); //convert into string & store it in multidimentional array
             }
         }
 
